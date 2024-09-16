0d11a99cff67b2edb165788c0b115dbf850adc7d
Kashish Pratap Singh <singhkashishpratap@gmail.com>
2024-09-16 23:12:03 +0530

    Some modification

--- SubTwoNumber.java
+++ SubTwoNumber.java
@@ -7,30 +7,35 @@ public class SubTwoNumber {
         
     }
 }
-class Sub extends JFrame implements ActionListener{
+class Sub extends JFrame {
     JTextField a;
     JTextField a1;
     JLabel j;
-    Button b;
+    JButton b;
     public Sub(){
         a=new JTextField(20);
         a1=new JTextField(20);
         setLayout(new FlowLayout());
         j=new JLabel("Result");
-        b=new Button("Ok");
+        b=new JButton("Sub");
         add(a);
         add(a1);
         add(b);
         add(j);
-        b.addActionListener(this);
+        ActionListener ac=new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                int num1=Integer.parseInt(a.getText());
+                int num2=Integer.parseInt(a1.getText());
+                int value=num1-num2;
+                j.setText(value+" ");
+            }
+        };
+        b.addActionListener(ac);
         setVisible(true);
         setSize(400,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    public void actionPerformed(ActionEvent ae){
-        int num1=Integer.parseInt(a.getText());
-        int num2=Integer.parseInt(a1.getText());
-        int value=num1-num2;
-        j.setText(value+" ");
-    }
+    
+    
+    
 }
